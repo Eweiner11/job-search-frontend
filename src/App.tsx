2b2a0f3e2e5e3aecf6312dd92c91e287ba74bcc3
Eric Weiner <Eweiner11@gmail.com>
2023-03-23 15:46:59 -0400

    connected to api

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,28 @@
 import * as React from 'react';
 import { hot } from "react-hot-loader/root";
 import Button from '@material-ui/core/Button';
+import { fetchTest } from './fetchers/fetchTest';
+
 
 interface Props {
-   name:
-    string
+  name:
+  string
 }
 
 class App extends React.Component<Props> {
+
+  
+
+  componentDidMount(): void {
+   fetchTest()
+  }
+
+
   render() {
     const { name } = this.props;
     return (
       <>
-       <h1>Job Search Front End</h1>
+        <h1>Job Search Front End</h1>
       </>
     );
   }
